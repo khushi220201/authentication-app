c09dd18979b5fe110694dbe66e25f10d20cd85a3
khushi <khushi220201@gmail.com>
2025-05-14 10:02:49 +0530

    added trim validation

--- authentication-app-backend/app/helpers/validators.ts
+++ authentication-app-backend/app/helpers/validators.ts
@@ -1,21 +1,22 @@
 import { body } from "express-validator";
 
 export const registerValidationRules = [
-  body("firstName").notEmpty().withMessage("First name is required"),
-  body("lastName").notEmpty().withMessage("Last name is required"),
-  body("email").isEmail().withMessage("Valid email is required"),
+  body("firstName").trim().notEmpty().withMessage("First name is required"),
+  body("lastName").trim().notEmpty().withMessage("Last name is required"),
+  body("email").trim().isEmail().withMessage("Valid email is required"),
   body("password")
+    .trim()
     .isLength({ min: 6 }).withMessage("Password must be at least 6 characters")
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/)
     .withMessage("Password must include uppercase, lowercase, and a number"),
 ];
 
 export const loginValidationRules = [
-  body("email").isEmail().withMessage("Valid email is required"),
-  body("password").notEmpty().withMessage("Password is required"),
+  body("email").trim().isEmail().withMessage("Valid email is required"),
+  body("password").trim().notEmpty().withMessage("Password is required"),
 ];
 
 export const verifyCodeValidationRule = [
-  body("email").isEmail(),
-  body("code").notEmpty().withMessage("Verification code is required"),
+  body("email").trim().isEmail().withMessage("Valid email is required"),
+  body("code").trim().notEmpty().withMessage("Verification code is required"),
 ];
